47e084a64b1ae7a353fcfea19db427c3e5e66fe4
celestialcreature <spvceiscool@gmail.com>
2023-11-07 14:06:42 -0600

    now prints the character's information

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Just a little project to do to practice things
 <br><br>
 Accepts a character file containing character information and uses that information to run a small battle simulation
 <br><br>
-Character file format:<br>
+Character file format: "stareaters/SE_name.txt"<br>
 Name-<br>
 Level-<br>
 Core-<br>
--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import os
 def main():
     character = select_character_file()
     character_file = open_character_file(character)
-    print(get_character_information(character_file))
+    Character_information = get_character_information(character_file)
+
+    print(f"\nYour selected Star Eater is {Character_information['Name']}!\n")
+    print(f"{Character_information['Name']} is a {Character_information['Size']}, {Character_information['Core']} core Star Eater")
+    print(f"The unique feature they possess is: {Character_information['Special']}")
 
 def select_character_file(): 
     name = input("enter character name: ").lower()
